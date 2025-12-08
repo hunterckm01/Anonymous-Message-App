efaea6a44207b1c246bb83e35af14897f00024a3
hunterckm01 <huner6894@gmail.com>
2025-12-08 23:50:45 +0530

    Send Email functionality with Resend is formed

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,33 @@
+import { resend } from "@/lib/resesnd";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/apiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string, 
+    verifyCode: string
+):Promise<ApiResponse>{
+    try{
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Anonymous Message | Verification Code',
+            react: VerificationEmail({username, 
+              otp:verifyCode
+             })
+        })
+
+        return ({
+            success: true,
+            message: "Email Sent Successfully"
+        })
+
+    }   
+    catch(err){
+        console.error("Error Sending Verification Email");
+        return {
+            success: false,
+            message: 'Failed to send verification email'
+        }
+    }
+}
